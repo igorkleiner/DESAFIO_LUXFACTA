84610c2afd36ea7af078b9b1a2edccc06e482cde
igorkleiner <igor_kleiner@hotmail.com>
2019-02-05 13:29:03 -0200

    Update database.php
    alterado senha para novo host

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'desafio_luxfacta',
 			'username'  => 'root',
-			'password'  => 'root',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',

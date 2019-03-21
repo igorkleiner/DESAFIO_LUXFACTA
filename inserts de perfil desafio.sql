44e9c0da4ef8ea09d9cb710fd2f0f54a85cc4894
igorkleiner <igor_kleiner@hotmail.com>
2019-03-21 15:16:27 -0300

    Update dos scripts de criação de base

--- inserts de perfil desafio.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: SELECT * FROM desafio_luxfacta.perfil
--- Date: 2017-07-03 16:33
-*/
-INSERT INTO `perfil` (`per_id`,`per_nome`) VALUES (0,'Guest');
-INSERT INTO `perfil` (`per_id`,`per_nome`) VALUES (1,'Cliente');
-INSERT INTO `perfil` (`per_id`,`per_nome`) VALUES (2,'Funcionario');
-INSERT INTO `perfil` (`per_id`,`per_nome`) VALUES (3,'Gerente');
-INSERT INTO `perfil` (`per_id`,`per_nome`) VALUES (4,'Dono');

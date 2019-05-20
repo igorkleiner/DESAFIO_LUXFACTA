0e363e5a241ae16c516ae9ad090af7e88d611264
igorkleiner <igor_kleiner@hotmail.com>
2019-05-20 09:46:46 -0300

    Update footer.blade.php

--- Apresentacao/app/views/layout/footer.blade.php
+++ Apresentacao/app/views/layout/footer.blade.php
@@ -1,3 +1,3 @@
 <script type="text/javascript" src="{{asset('assets/bootstrap-3.3.6-dist/js/bootstrap.js')}}"></script> 
-<script type="text/javascript" src="{{ asset('assets/bootstrap-3.3.6-dist/js/bootstrap-datepicker.js') }}"></script>65
-<script type="text/javascript" src="{{ asset('assets/bootstrap-3.3.6-dist/js/bootstrap-datepicker.pt-BR.js') }}"></script>65
+<script type="text/javascript" src="{{ asset('assets/bootstrap-3.3.6-dist/js/bootstrap-datepicker.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/bootstrap-3.3.6-dist/js/bootstrap-datepicker.pt-BR.js') }}"></script>
